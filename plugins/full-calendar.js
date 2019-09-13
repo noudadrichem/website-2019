e3e3078225a5bfc27bb6249cd84556c553b92251
joostlek <joostlek@outlook.com>
2019-09-13 14:11:41 +0200

    Fix Calendar

--- plugins/full-calendar.js
+++ plugins/full-calendar.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Vue from 'vue'
-import Calendar from '../components/calendar'
+import Calendar from '../components/Calendar'
 
 Vue.component('full-calendar', Calendar)
