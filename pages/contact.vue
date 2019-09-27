7bf789b6b5069621e2385378e4609df7dae7ed46
joostlek <joostlek@outlook.com>
2019-09-27 20:51:06 +0200

    Fix contact page responsiveness

--- pages/contact.vue
+++ pages/contact.vue
@@ -11,12 +11,15 @@
           <li>bestuur@indicium.hu</li>
         </ul>
 
+        <p>KVK: 68788878</p>
         <p>Of stuur direct een email naar het bestuur:</p>
-        <Button size="l" url="mailto:bestuur@indicium.hu">Mail direct</Button>
+        <Button size="l" url="mailto:bestuur@indicium.hu">
+          Mail direct
+        </Button>
       </div>
       <div class="column right">
         <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.7310543306226!2d5.173675415856721!3d52.08462557973417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6693e9a2d0c9f%3A0xa9867c6558813da4!2sHogeschool%20Utrecht%2C%20Heidelberglaan%2015%2C%203584%20CS%20Utrecht!5e0!3m2!1sen!2snl!4v1568892960255!5m2!1sen!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.7310543306226!2d5.173675415856721!3d52.08462557973417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6693e9a2d0c9f%3A0xa9867c6558813da4!2sHogeschool%20Utrecht%2C%20Heidelberglaan%2015%2C%203584%20CS%20Utrecht!5e0!3m2!1sen!2snl!4v1568892960255!5m2!1sen!2snl" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
       </div>
     </div>
@@ -35,17 +38,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '../assets/scss/variables.scss';
 .contact {
   padding-bottom: 64px;
 
   .flex {
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
   }
 
   .map {
     border-radius: 4px;
     position: relative;
+
+    iframe {
+      width: 600px;
+      height: 450px;
+      @media screen and (max-width: $bp-tablet-sm) {
+        width: 80vw;
+      }
+    }
+
+    @media screen and (max-width: $bp-tablet-sm) {
+      margin-top: 16px;
+    }
 
     &:after {
       content: '';
